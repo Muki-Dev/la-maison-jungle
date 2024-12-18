84cc9e9e658c9e880f6b8cca94ea68c830086c7f
Muki <mukidev881@gmail.com>
2024-12-18 12:32:06 +0100

    modify banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Banner from './components/Banner';
 import Cart from './components/Cart';
 import ShoppingList from './components/ShoppingList';
+import logo from './assets/logo.png'
 
 
 function App() {
   return (
     <div>
-      <Banner />
+       <Banner>
+          <img className='lmj-logo' src ={logo} alt='lmj-logo' />
+          <h1 className='lmj-title'>La maison Jungle</h1>
+       </Banner>
       <Cart />
       <ShoppingList />
     </div>
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,8 @@
 import logo from '../assets/logo.png'
 import '../styles/Banner.css'
-function Banner(){
-    const title = 'la maison jungle'
+function Banner({children}){
     return(
-        <div className='lmj-banner'>
-            <img className='lmj-logo' src={logo} alt="la-maison-jungle" />
-            <h1 className='lmj-title'>{title.toUpperCase()}</h1>
-        </div>
+        <div className='lmj-banner'>{children}</div>
     )
 }
 
