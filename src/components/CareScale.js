cbe8e3ff5021ce7b2f81843ad2eef3af7b2c2c33
Muki <mukidev881@gmail.com>
2024-12-18 13:17:03 +0100

    event onclick

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,14 +1,23 @@
 import sun from '../assets/sun.svg';
 import water from '../assets/water.svg';
+const quantityLabel = {
+    1:'peu',
+    2:'moderement',
+    3:'beaucoup'
+}
 
 function CareScale({scaleValue,careType}){
     const range = [1,2,3];
     const scaleType = careType === 'light' ? (<img src={sun} alt='sun-icon' />) : (<img src={water} alt='water-icon' />)
     return(
-        <div>
+        <div
+            onClick={() => alert(
+                `Cette plante requiert ${quantityLabel[scaleValue]} ${careType === "light" ? "de lumiere" : "d'arrosage"}`
+            )}
+        >
             {
                 range.map((rangeElem) => (
-                    rangeElem >= scaleValue ? <span>{scaleType}</span> : null
+                    scaleValue >= rangeElem ? (<span key={rangeElem.toString()}>{scaleType}</span>) : (null)
                 ))
             }
         </div>
