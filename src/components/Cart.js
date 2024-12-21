daa40b5a940c9a75a49cccd515cece5b20ad6027
Muki <mukidev881@gmail.com>
2024-12-21 19:08:21 +0100

    add state at cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,20 +1,28 @@
+import { useState } from 'react'
 import '../styles/Cart.css'
 function Cart(){
     const monsteraPrice = 8;
-    const ivyPrice = 10;
-    const flowersPrice = 20;
-    const total = monsteraPrice + ivyPrice + flowersPrice
-    return(
+    const [cart,updateCart] = useState(0);
+    const [isOpen,setIsOpen] = useState(false)
+   
+    
+    return isOpen ? (
         <div className='lmj-cart'>
+            <button className='lmj-cart-toggle-button' onClick={() => setIsOpen(false)}>Fermer</button>
             <h2>Panier</h2>
-            <ul>
-                <li>Monstera:{monsteraPrice}£</li>
-                <li>ivyPrice:{ivyPrice}£</li>
-                <li>bouquet de fleur:{flowersPrice}£</li>
-            </ul>
-            <h3>total: {total}£</h3>
+            <div>
+                Monstera:{monsteraPrice}€
+                <button onClick={() => updateCart(cart + 1)}>Ajouter</button>
+            </div>
+            <h3>total: {cart * monsteraPrice}€</h3>
+            <button onClick={() => updateCart(0)}>Vider le Panier</button>
         </div>
-    )
+    ):(
+        <div className='lmj-cart-closed'>
+             <button className='lmj-cart-toggle-button' onClick={() => setIsOpen(true)}>Ouvrir le panier</button>
+        </div>
+   
+)
 }
 
 export default Cart;
