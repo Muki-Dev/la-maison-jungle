c3d3582a08dcfdfb5cd217ce10133931bec56f69
Muki <mukidev881@gmail.com>
2024-12-21 20:39:09 +0100

    share state between cart and shopping component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
+import { useState } from 'react';
 import Banner from './components/Banner';
 import Cart from './components/Cart';
 import ShoppingList from './components/ShoppingList';
 import logo from './assets/logo.png'
 import Footer from './components/Footer';
+import './styles/Layout.css';
 
 
 function App() {
+  const [cart,updateCart] = useState([])
   return (
     <div>
        <Banner>
           <img className='lmj-logo' src ={logo} alt='lmj-logo' />
           <h1 className='lmj-title'>La maison Jungle</h1>
        </Banner>
-      <Cart />
-      <ShoppingList />
+       <div className='lmj-layout-inner'>
+          <Cart cart={ cart } updateCart={ updateCart } />
+          <ShoppingList cart={ cart } updateCart={ updateCart } />
+       </div>
       <Footer />
     </div>
   );
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,10 +2,20 @@ import { plantList } from "../datas/plantList";
 import PlantItem from "./PlantItem";
 import '../styles/ShoppingList.css';
 
-function ShoppingList(){
+function ShoppingList({cart,updateCart}){
     const categories = plantList.reduce((acc,elem) =>
             acc.includes(elem.category) ? acc : acc.concat(elem.category), []
     )
+
+    function addToCart(name,price){
+        const plantSaved = cart.find((plant) => plant.name === name )
+        if(plantSaved){
+            const filteredPlantSaved = cart.filter((plant) => plant.name !== name)
+            updateCart([...filteredPlantSaved,{name,price,amount: plantSaved.amount + 1}])
+        }else{
+            updateCart([...cart,{name,price,amount:1}])
+        }
+    }
     return(
         <div>
             <ul>
@@ -17,15 +27,14 @@ function ShoppingList(){
             </ul>
             <ul className="lmj-plant-list">
                 {
-                    plantList.map(({cover,name,id,light,water}) => (
-                        <PlantItem 
-                         cover={cover}
-                         name={name}
-                         id={id}
-                         light={light}
-                         water={water}
-                        />
+                    plantList.map(({cover,name,id,light,water,price}) => (
+                       <div key={id}>
+                            <PlantItem cover={cover} name={name} id={id} light={light} water={water} />
+                            <button onClick={() => addToCart(name,price)}>Ajouter</button>
+                       </div> 
+                        
                     ))
+                   
                 }
             </ul>
         </div>
