63ff482ed4944a80914461278b3aad659f474e8b
Muki <mukidev881@gmail.com>
2024-12-25 13:15:04 +0100

    selected multiple category

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,26 +1,25 @@
-import '../styles/Categories.css'
-function Categories({activeCategory,setActiveCategory,categories}){
-    return(
-        <div className="lmj-categories">
-        <select
-                value={activeCategory}
-                onChange={(e) => setActiveCategory(e.target.value)}
-                className="lmj-categories-select"
-            >
-                <option value=''>---</option>
-                {
-                    categories.map((cat) => (
-                        <option
-                        value={cat}
-                        key={cat}
-                        >{cat}</option>
-                    ))
-                }
-            </select>
-            <button onClick={() => setActiveCategory('')}>Renitialiser</button>
-        </div>
-        
-    )
-}
+function Categories({ activeCategory, categories, onToggle }) {
+    return (
+      <div className="lmj-categories">
+        <ul>
+          {categories.map((cat) => (
+            <li key={cat}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={activeCategory.includes(cat)}
+                  onChange={() => onToggle(cat)}
+                />
+                {cat}
+              </label>
+            </li>
+          ))}
+        </ul>
+        <button onClick={() => activeCategory.splice(0, activeCategory.length)}>
+          Réinitialiser
+        </button>
+      </div>
+    );
+  }
 
-export default Categories
+  export default Categories;
