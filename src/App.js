84fc20a586b3104174a1bbc0cc53c78082f7fa1e
Muki <mukidev881@gmail.com>
2025-01-16 14:35:52 +0100

    remove props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Provider } from 'react-redux';
 import store from './app/store';
-import { useState } from 'react';
 import Banner from './components/Banner';
 import Cart from './components/Cart';
 import ShoppingList from './components/ShoppingList';
@@ -9,7 +8,6 @@ import Footer from './components/Footer';
 import './styles/Layout.css';
 
 function App() {
-  const [cart,updateCart] = useState([])
   return (
     <Provider store={store}>
       <div>
@@ -18,8 +16,8 @@ function App() {
             <h1 className='lmj-title'>La maison Jungle</h1>
         </Banner> 
         <div className='lmj-layout-inner'>
-            <Cart cart={ cart } updateCart={ updateCart } />
-            <ShoppingList  cart={ cart } updateCart={ updateCart } />
+            <Cart />
+            <ShoppingList />
         </div>
         <Footer />
       </div>
