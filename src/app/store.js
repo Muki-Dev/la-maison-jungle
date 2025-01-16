8666756b6a39c11cbe2f6047176687309db5531e
Muki <mukidev881@gmail.com>
2025-01-16 14:50:26 +0100

    add categorySlice to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
  import { configureStore } from "@reduxjs/toolkit";
  import cartReducer from '../features/CartSlice';
+ import CategoryReducer from '../features/CategorySlice'
 
  const store = configureStore({
    reducer:{
       cart: cartReducer,
+      category: CategoryReducer,
    }
  })
 
