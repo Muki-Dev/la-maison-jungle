aec4297df5c54684df5695cdd6a601c88eb365d5
Muki <mukidev881@gmail.com>
2025-01-16 15:19:11 +0100

    filter category with redux/toolkit

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,14 +1,14 @@
-import { useDispatch } from "react-redux";
+import { useDispatch,useSelector } from "react-redux";
 import { addToCart, removeToCart } from "../features/CartSlice";
-import { useState } from "react";
 import { plantList } from "../datas/plantList";
 import PlantItem from "./PlantItem";
 import Categories from "./Categories";
 import "../styles/ShoppingList.css";
 
+
 function ShoppingList() {
-  const [activeCategory, setActiveCategory] = useState([]);
   const dispatch = useDispatch();
+  const activeCategories = useSelector((state) => state.category.activeCategories)
 
   const categories = plantList.reduce(
     (acc, elem) =>
@@ -16,30 +16,18 @@ function ShoppingList() {
     []
   );
 
-  const toggleCategory = (category) => {
-    if (activeCategory.includes(category)) {
-      // Supprimer la catégorie si elle est déjà sélectionnée
-      setActiveCategory(activeCategory.filter((cat) => cat !== category));
-    } else {
-      // Ajouter la catégorie si elle n'est pas encore sélectionnée
-      setActiveCategory([...activeCategory, category]);
-    }
-  };
-
+ 
   return (
     <div className="lmj-shopping-list">
        <Categories
-        activeCategory={activeCategory}
-        setActiveCategory={setActiveCategory}
         categories={categories}
-        onToggle={toggleCategory}
       />  
 
       <ul className="lmj-plant-list">
         {plantList
           .filter(
             ({ category }) =>
-              activeCategory.length === 0 || activeCategory.includes(category)
+              activeCategories.length === 0 || activeCategories.includes(category)
           )
           .map(({ cover, name, id, light, water, price,description }) => (
             <div key={id}>
