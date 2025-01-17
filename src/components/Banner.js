59ab1c08ca6f9e9b5238614bbea8e96b41c3f8dd
Muki <mukidev881@gmail.com>
2025-01-18 00:08:59 +0100

    add react router link

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,16 @@
-import '../styles/Banner.css'
-function Banner({children}){
-    return(
-        <div className='lmj-banner'>{children}</div>
-    )
+import { Link } from "react-router-dom";
+import "../styles/Banner.css";
+
+function Banner({ children }) {
+  return (
+    <div className="lmj-banner">
+      {children}
+      <div className="lmj-nav-links">
+        <Link to="/login" className="lmj-link">Connexion</Link>
+        <Link to="/register" className="lmj-link">Inscription</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
