360db02d717b3763c2dbb9446b7dbeedf45dfba0
Muki <mukidev881@gmail.com>
2025-01-18 00:29:02 +0100

    update banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,11 +4,11 @@ import "../styles/Banner.css";
 function Banner({ children }) {
   return (
     <div className="lmj-banner">
-      {children}
-      <div className="lmj-nav-links">
+      <div className="lmj-banner-title">{children}</div>
+      <nav className="lmj-banner-nav">
         <Link to="/login" className="lmj-link">Connexion</Link>
         <Link to="/register" className="lmj-link">Inscription</Link>
-      </div>
+      </nav>
     </div>
   );
 }
