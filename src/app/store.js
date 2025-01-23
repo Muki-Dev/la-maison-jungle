2361213da65762e4a464ef141775560127391d10
Muki <mukidev881@gmail.com>
2025-01-23 11:41:53 +0100

    modify and delete file

--- src/app/store.js
+++ src/app/store.js
@@ -2,14 +2,12 @@
  import cartReducer from '../features/CartSlice';
  import categoryReducer from '../features/CategorySlice';
  import modalReducer from '../features/modalSlice';
- import userReducer from '../features/userSlice';
 
  const store = configureStore({
    reducer:{
       modal: modalReducer,
       cart: cartReducer,
       category: categoryReducer,
-      user: userReducer
    }
  })
 
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,25 +1,13 @@
 import { Link } from "react-router-dom";
-import { useDispatch} from 'react-redux';
-import { useNavigate} from 'react-router-dom';
-import { logout } from '../features/userSlice';
 import "../styles/Banner.css";
 
 function Banner({ children }) {
-  const dispatch = useDispatch();
-const navigate = useNavigate() 
-
-  const handleLogout = () => {
-      dispatch(logout());
-      navigate('/login'); // Redirige vers la page de connexion
-  };
-
   return (
     <div className="lmj-banner">
       <div className="lmj-banner-title">{children}</div>
       <nav className="lmj-banner-nav">
         <Link to="/login" className="lmj-link">Connexion</Link>
-        <Link to="/register" className="lmj-link">Inscription</Link>
-        <button onClick={handleLogout}>Déconnexion</button> 
+        <Link to="/register" className="lmj-link">Inscription</Link> 
       </nav>
     </div>
   );
