d2682d4bcb53b52d2210c78d5808ddf341152fd9
Muki <mukidev881@gmail.com>
2025-01-23 11:49:57 +0100

    add AuthReducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -2,9 +2,11 @@
  import cartReducer from '../features/CartSlice';
  import categoryReducer from '../features/CategorySlice';
  import modalReducer from '../features/modalSlice';
+ import AuthReducer from '../features/AuthSlice';
 
  const store = configureStore({
    reducer:{
+      auth: AuthReducer,
       modal: modalReducer,
       cart: cartReducer,
       category: categoryReducer,
